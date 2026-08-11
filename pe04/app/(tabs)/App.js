dc56cce711e1449a1788152a39f9d61461e22644
shinoacute <167141772+shinoacute@users.noreply.github.com>
2026-08-11 18:59:53 +0000

    pe04-2

--- pe04/app/(tabs)/App.js
+++ pe04/app/(tabs)/App.js
@@ -1,76 +1,99 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import update from 'immutability-helper';
+import { Image, Text, View, TouchableHighlight } from 'react-native';
+import styles from './styles';
+
+const userImage = require('../../assets/images/user.png');
+
+const userProfileDetails = {
+  name: 'Thao Bui',
+  occupation: 'React Developer',
+  description: 'Thao is a React Native learner. She likes learning about React Native and developing mobile applications.',
+};
+
+const numberOfCards = 6;
+const data = Array(numberOfCards).fill(null).map((_, index) => ({
+  image: userImage,
+  ...userProfileDetails,
+  showThumbnail: true,
+}));
+
+const ProfileCard = ({ image, name, occupation, description, onPress, showThumbnail }) => {
+  const cardContainerStyles = [styles.cardContainer];
+  if (showThumbnail) {
+    cardContainerStyles.push(styles.cardThumbnail);
+  }
+
+  return (
+    <TouchableHighlight
+      style={cardContainerStyles}
+      onPress={onPress}
+      underlayColor="rgba(0,0,0,0.05)"
+    >
+      <View style={styles.cardContentWrapper}>
+        <View style={styles.cardImageContainer}>
+          <Image style={styles.cardImage} source={image} />
+        </View>
+        <View style={styles.textContentContainer}>
+          <Text style={[styles.cardName, showThumbnail && styles.thumbnailCardName]}>{name}</Text>
+          <View style={styles.cardOccupationContainer}>
+            <Text style={[styles.cardOccupation, showThumbnail && styles.thumbnailCardOccupation]}>
+              {occupation}
+            </Text>
+          </View>
+          <Text
+            style={[styles.cardDescription, showThumbnail && styles.thumbnailCardDescription]}
+            ellipsizeMode={showThumbnail ? 'tail' : undefined}
+          >
+            {description}
+          </Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+ProfileCard.propTypes = {
+  image: PropTypes.any.isRequired,
+  name: PropTypes.string.isRequired,
+  occupation: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
+  showThumbnail: PropTypes.bool.isRequired,
+};
 
 export default class App extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.cardImageContainer}>
-                        <Image style={styles.cardImage}
-                            source={require('../../assets/images/user.png')}/>
-                    </View>
-                    <Text style={styles.cardName}>John Doe</Text>
-                    <Text style={styles.cardTitle}>React Native Developer</Text>
-                    <Text style={styles.cardBio}>
-                        John is a really great JavaScript developer. He loves using JS to build React Native applications for iOS and Android.
-                    </Text>
-                </View>
-            </View>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { data: data };
+  }
+
+  handleProfileCardPress = (selectedIndex) => {
+    const { data } = this.state;
+    const currentlySelectedIsThumbnail = data[selectedIndex].showThumbnail;
+    const newData = data.map((item, index) => {
+      if (index === selectedIndex) {
+        return update(item, { showThumbnail: { $set: !item.showThumbnail } });
+      } else if (currentlySelectedIsThumbnail) {
+        return update(item, { showThumbnail: { $set: true } });
+      } else {
+        return item;
+      }
+    });
+    this.setState({ data: newData });
+  };
+
+  render() {
+    const { data } = this.state;
+    const list = data.map((item, index) => (
+      <ProfileCard key={`card-${index}`} {...item} onPress={() => this.handleProfileCardPress(index)} />
+    ));
+
+    return (
+      <View style={styles.appContainer}>
+        <View style={styles.galleryContainer}>{list}</View>
+      </View>
+    );
+  }
 }
-
-const profileCardColor = 'dodgerblue';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    cardContainer: {
-        alignItems: 'center',
-        borderColor: 'black',
-        borderWidth: 3,
-        borderStyle: 'solid',
-        borderRadius: 20,
-        backgroundColor: profileCardColor,
-        width: 300,
-        height: 400,
-        padding: 15
-    },
-    cardImageContainer: {
-        alignItems: 'center',
-        backgroundColor: 'white',
-        borderWidth: 3,
-        borderColor: 'black',
-        width: 120,
-        height: 120,
-        borderRadius: 60,
-        marginTop: 30,
-        paddingTop: 15
-    },
-    cardImage: {
-        width: 80,
-        height: 80
-    },
-    cardName: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginTop: 20,
-        color: 'white'
-    },
-    cardTitle: {
-        fontSize: 14,
-        marginTop: 5,
-        textDecorationLine: 'underline',
-        fontWeight: 'bold',
-    },
-    cardBio: {
-        fontSize: 13,
-        textAlign: 'justify',
-        marginTop: 15,
-        paddingHorizontal: 10
-    }
-});
--- pe04/app/(tabs)/styles.js
+++ pe04/app/(tabs)/styles.js
@@ -1,46 +1,132 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet, Dimensions, Platform  } from 'react-native'
 
-export const Colors = {
-    dark: 'black',
-    light: 'white'
-};
+const screenHeight = Dimensions.get('window').height;
+const statusBarHeight = Platform.OS === 'ios' ? 40 : 20;
+const availableHeight = screenHeight - statusBarHeight - 40;
+const expandedCardHeight = Math.min(230, availableHeight / 3 * 0.9);
 
-const baseContainerStyles = {
+const styles = StyleSheet.create({
+  appContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-};
-
-const baseBoxStyles = {
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: statusBarHeight + 20,
+    paddingBottom: 20,
     alignItems: 'center',
-    borderWidth: 2,
-    height: 150,
-    width: 150
-};
-
-const lightStyleSheet = StyleSheet.create({
-    container: {
-        ...baseContainerStyles,
-        backgroundColor: Colors.light
-    },
-    box: {
-        ...baseBoxStyles,
-        borderColor: Colors.dark
-    }
+  },
+  galleryContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '90%',
+    maxWidth: 340, 
+    justifyContent: 'center', 
+  },
+  cardContainer: { 
+    flexBasis: '50%', 
+    paddingHorizontal: 3, 
+    marginVertical: 3,
+    alignItems: 'center', 
+    justifyContent: 'center',
+    ...Platform.select({ 
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        elevation: 15
+      }
+    })
+  },
+  cardContentWrapper: { 
+    width: '100%', 
+    height: expandedCardHeight,
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 2.5,
+    borderRadius: 18,
+    backgroundColor: 'dodgerblue',
+    paddingVertical: 8,
+    paddingHorizontal: 6,
+    overflow: 'hidden',
+  },
+  cardImageContainer: {
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 5,
+    ...Platform.select({ 
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10,
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        elevation: 15
+      }
+    }) 
+  },
+  cardImage: {
+    width: 50,
+    height: 50,
+  },
+  textContentContainer: {
+    marginTop: 10,
+    alignItems: 'center',
+    width: '100%',
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingBottom: 3,
+  },
+  cardName: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  thumbnailCardName: {
+    fontSize: 12,
+  },
+  cardOccupationContainer: {
+    borderColor: 'black',
+    borderBottomWidth: 1.2,
+    paddingBottom: 1,
+    marginBottom: 3,
+    alignSelf: 'stretch',
+    marginHorizontal: '12%',
+  },
+  cardOccupation: {
+    color: 'black',
+    fontWeight: '500',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  thumbnailCardOccupation: {
+    fontSize: 10,
+  },
+  cardDescription: {
+    color: 'black',
+    fontSize: 10,
+    textAlign: 'center',
+    paddingHorizontal: 2,
+    marginTop: 2,
+    fontStyle: 'italic',
+  },
+  thumbnailCardDescription: {
+    fontSize: 8,
+  },
+  cardThumbnail: {
+    transform: [{ scale: 0.5}],
+  },
 });
 
-const darkStyleSheet = StyleSheet.create({
-    container: {
-        ...baseContainerStyles,
-        backgroundColor: Colors.dark
-    },
-    box: {
-        ...baseBoxStyles,
-        borderColor: Colors.light
-    }
-});
-
-export default function getStyleSheet(useDarkTheme){
-    return useDarkTheme ? darkStyleSheet : lightStyleSheet;
-}
+export default styles;
