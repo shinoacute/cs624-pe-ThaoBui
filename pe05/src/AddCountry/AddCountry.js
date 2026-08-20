816346a026155adb5a1661355a05741ec229301d
shinoacute <167141772+shinoacute@users.noreply.github.com>
2026-08-20 21:21:48 +0000

    PE07

---
+++ pe05/src/AddCountry/AddCountry.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import uuid from 'react-native-uuid';
+import { colors } from '../theme';
+
+class AddCountry extends React.Component {
+  state = {
+    country: '',
+    currency: '',
+  };
+
+  onChangeText = (key, value) => {
+    this.setState({ [key]: value });
+  };
+
+  submit = () => {
+    const { country, currency } = this.state;
+    if (country.trim() === '' || currency.trim() === '') {
+      alert('please complete form');
+      return;
+    }
+
+    this.props.addCountry({
+      country: country.trim(),
+      currency: currency.trim(),
+      id: uuid.v4(),
+    });
+    this.setState({ country: '', currency: '' }, () => {
+      this.props.navigation.navigate('Countries');
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>Countries</Text>
+        <TextInput
+          placeholder="Country name"
+          onChangeText={(value) => this.onChangeText('country', value)}
+          style={styles.input}
+          value={this.state.country}
+        />
+        <TextInput
+          placeholder="Currency"
+          onChangeText={(value) => this.onChangeText('currency', value)}
+          style={styles.input}
+          value={this.state.currency}
+        />
+        <TouchableOpacity onPress={this.submit}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Add Country</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    height: 50,
+    backgroundColor: '#666',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  heading: {
+    color: 'white',
+    fontSize: 40,
+    marginBottom: 10,
+    alignSelf: 'center',
+  },
+  container: {
+    backgroundColor: colors.primary,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    margin: 10,
+    backgroundColor: 'white',
+    paddingHorizontal: 8,
+    height: 50,
+  },
+});
+
+export default AddCountry;
